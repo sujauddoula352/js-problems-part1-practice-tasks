bd415b4978240b6fbc281aea37faff0e2ba218b1
SH_SOHEL <sujauddoulasohel352@gmail.com>
2024-02-04 15:46:35 +0600

    convert temperature from celsius to fahrenheit

---
+++ task1.js
@@ -0,0 +1,13 @@
+/**# js-problems-part1-practice-tasks
+
+### Task-1: 
+Write a function to convert temperature from Celsius to Fahrenheit.
+
+--- */
+function temperature(Celsius){
+    const fahrenheit=(Celsius*1.8)+32;
+  return fahrenheit;
+  
+}
+const convert=temperature(0);
+console.log(convert);
